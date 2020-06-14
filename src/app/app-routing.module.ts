a337110fbb0e19204813e0cff352d32d98c32a14
mrwersa <aghaeester@gmail.com>
2020-06-14 18:52:44 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then((m) => m.LoginPageModule),
+  },
+  {
+    path: "tabs",
+    loadChildren: () =>
+      import("./pages/tabs/tabs.module").then((m) => m.TabsPageModule),
+  },
+  {
+    path: "register",
+    loadChildren: () =>
+      import("./pages/register/register.module").then(
+        (m) => m.RegisterPageModule
+      ),
+  },
+  {
+    path: "chat-view",
+    loadChildren: () =>
+      import("./pages/chat-view/chat-view.module").then(
+        (m) => m.ChatViewPageModule
+      ),
+  },
+
+  { path: "**", redirectTo: "/login" },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
